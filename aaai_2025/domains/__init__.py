90eb01cbb93f804ebfb5ed9cec39b5206a997aad
Roland Godet <r.godet@outlook.fr>
2024-07-19 16:42:30 +0200

    feat(aaai25): add rcpsp

--- aaai_2025/domains/__init__.py
+++ aaai_2025/domains/__init__.py
@@ -1,5 +1,6 @@
 from .depots import Aaai2025DepotsDomain
 from .jobshop import Aaai2025JobshopDomain
 from .match_cellar_ice import Aaai2025MatchCellarIceDomain
+from .rcpsp import Aaai2025RcpspDomain
 from .rovers import Aaai2025RoversDomain
 from .satellite import Aaai2025SatelliteDomain
